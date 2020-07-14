63ae1fdb880db7552f057c4c7c90c7dc27a44a25
Alex Dewar <a.dewar@sussex.ac.uk>
2020-07-14 15:45:18 +0100

    Show route IDFs for Newton database

---
+++ navbench.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import os
+import pandas as pd
+
+
+# Read info for image database entries from CSV file
+def read_image_database(path):
+    df = pd.read_csv(os.path.join(path, "database_entries.csv"))
+    df = df.rename(columns=lambda x: x.strip())  # strip whitespace
+
+    entries = {
+        "x": df["X [mm]"] / 1000,
+        "y": df["Y [mm]"] / 1000,
+        "z": df["Z [mm]"] / 1000,
+        "heading": df["Heading [degrees]"],
+        "filepath": [os.path.join(path, fn.strip()) for fn in df["Filename"]]
+    }
+
+    return entries
+
+# Returns a greyscale image of type uint8
+def read_image(path):
+    im = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    assert (im.dtype == np.uint8)
+    return im
+
+
+def image_diff(im1, im2):
+    d = cv2.absdiff(im1, im2)
+    return d.mean() / 255
+
+
+def get_route_idf(images, snap):
+    return [image_diff(im, snap) for im in images]
+
+
+def get_ridf(image, snap):
+    diffs = []
+    for _ in range(image.shape[1]):
+        diffs.append(image_diff(image, snap))
+        image = np.roll(image, 1, axis=1)
+    return diffs
+
+
+def get_route_ridf(images, snap):
+    return [min(get_ridf(im, snap)) for im in images]
+
