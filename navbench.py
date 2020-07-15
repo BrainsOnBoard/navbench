d4f17c6757f17c44e9ed7f99df1716f7ab9aabcd
Alex Dewar <a.dewar@sussex.ac.uk>
2020-07-15 16:40:29 +0100

    Test multiple locations along route

--- navbench.py
+++ navbench.py
@@ -1,6 +1,9 @@
+import math
+import os
+
 import cv2
 import numpy as np
-import os
+import matplotlib.pyplot as plt
 import pandas as pd
 
 
@@ -19,6 +22,7 @@ def read_image_database(path):
 
     return entries
 
+
 def read_image(path, size=()):
     """Returns a greyscale image of type float."""
     im = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
@@ -28,7 +32,6 @@ def read_image(path, size=()):
     im = im.astype(np.float) / info.max
 
     return cv2.resize(im, size) if size else im
-
 
 
 def read_images(paths, size=()):
@@ -60,6 +63,7 @@ def mean_absdiff(x, y):
     absdiffs = np.abs(x - y)
     return absdiffs.mean(axis=(0, 1))
 
+
 def get_route_idf(images, snap):
     return mean_absdiff(images, snap)
 
@@ -79,3 +83,70 @@ def get_ridf(image, snap, step=1):
 
 def get_route_ridf(images, snap, step=1):
     return np.amin(get_ridf(images, snap, step), axis=0)
+
+
+class Database:
+    def __init__(self, path, size=(), step=1):
+        self.entries = read_image_database(os.path.join('databases', path))
+        self.size = size
+        self.step = step
+
+    def get_distance(self, i, j):
+        '''Euclidean distance between two database entries (in m)'''
+        dy = self.entries["y"][j] - self.entries["y"][i]
+        dx = self.entries["x"][j] - self.entries["x"][i]
+        return math.hypot(dy, dx)
+
+    def get_distances(self, from_entry, to_entry, ref_entry):
+        assert to_entry >= from_entry
+
+        dists = []
+        for i in range(from_entry, to_entry, self.step):
+            dist = self.get_distance(ref_entry, i)
+            dists.append(dist if i >= ref_entry else -dist)
+        return dists
+
+    def get_entry_bounds(self, max_dist, start_entry):
+        '''Get upper and lower bounds for frames > max_dist from start frame'''
+        upper_entry = start_entry
+        while self.get_distance(start_entry, upper_entry) < max_dist:
+            upper_entry += 1
+        lower_entry = start_entry - 1
+        while self.get_distance(start_entry, lower_entry) < max_dist:
+            lower_entry -= 1
+        return (lower_entry, upper_entry)
+
+    def read_images(self, from_entry, to_entry=None):
+        paths = self.entries["filepath"]
+        if to_entry is None:
+            return read_image(paths[from_entry], self.size)
+
+        return read_images(paths[from_entry:to_entry:self.step], self.size)
+
+    def plot_idfs(self, ax, ref_entry, max_dist, ridf_step=1):
+        (lower, upper) = self.get_entry_bounds(max_dist, ref_entry)
+        dists = self.get_distances(lower, upper, ref_entry)
+
+        # Load snapshot and test images
+        snap = self.read_images(ref_entry)
+        images = self.read_images(lower, upper)
+        print("Testing frames %i to %i (n=%i)" % (lower, upper, images.shape[2]))
+
+        # Show which part of route we're testing
+        x = self.entries["x"]
+        y = self.entries["y"]
+        ax[1].plot(x, y, x[lower:upper], y[lower:upper], x[ref_entry], y[ref_entry], 'ro')
+        ax[1].set_xlabel("x (m)")
+        ax[1].set_ylabel("y (m)")
+        ax[1].axis("equal")
+
+        # Plot (R)IDF diffs vs distance
+        idf_diffs = get_route_idf(images, snap)
+        ridf_diffs = get_route_ridf(images, snap, ridf_step)
+
+        ax[0].plot(dists, idf_diffs, dists, ridf_diffs)
+        ax[0].set_xlabel("Distance (m)")
+        ax[0].set_xlim(-max_dist, max_dist)
+        ax[0].set_xticks(range(-max_dist, max_dist))
+        ax[0].set_ylabel("Mean image diff (px)")
+        ax[0].set_ylim(0, 0.06)
