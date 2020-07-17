841359069b1c9449916df500df6d22dae56622fc
Alex Dewar <a.dewar@sussex.ac.uk>
2020-07-17 17:25:31 +0100

    First stab at sky removing algorithm

--- navbench/improc/__init__.py
+++ navbench/improc/__init__.py
@@ -1,10 +1,6 @@
 import cv2
 import numpy as np
 
-def histeq(im):
-    '''Histogram equalisation: Requires uint8s as input'''
-    return cv2.equalizeHist(im)
-
 def resize(width, height):
     '''Return a function to resize image to given size'''
     return lambda im: cv2.resize(im, (width, height))
@@ -17,7 +13,46 @@ def chain(*args):
         return im
     return chainedfun
 
+def histeq(im):
+    '''Histogram equalisation: Requires uint8s as input'''
+    return cv2.equalizeHist(im)
+
 def to_float(im):
     # Normalise values
     info = np.iinfo(im.dtype)
     return im.astype(np.float) / info.max
+
+def fill_holes(im):
+    '''Fills in any black objects'''
+    assert im.dtype == np.uint8
+    contours, _ = cv2.findContours(im, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        cv2.drawContours(im, [cnt], 0, 255, -1)
+    return im
+
+def threshold_otsu(im):
+    _, thresh = cv2.threshold(im, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    return thresh
+
+def get_ground_mask(im):
+    # First, roughly threshold image
+    im = threshold_otsu(im)
+
+    # Set bottom row to white so that any black bits below skyline will be
+    # filled in
+    im[-1,:] = 255
+
+    # Fill any black holes
+    im = fill_holes(im)
+
+    # Fill white holes, by inverting, filling then inverting again
+    im = cv2.bitwise_not(im)
+    im = fill_holes(im)
+    return cv2.bitwise_not(im)
+
+def remove_sky(im):
+    '''Sets the sky to 0'''
+    mask = get_ground_mask(im)
+    im = cv2.bitwise_and(im, mask)
+    return im
---
+++ threshold_db.py
@@ -0,0 +1,15 @@
+# Try to remove sky from a bunch of images and dump result in folder
+
+import cv2
+import navbench as nb
+from navbench import improc as ip
+
+for i in range(0, 1000, 100):
+    # Works really well on this dataset, but leaves some clouds in with the
+    # Newton dataset :-(
+    path = "databases/bottom_of_campus/straight_route2_fwd/frame%05i.jpg" % i
+    print(path)
+
+    im = nb.read_images(path)
+    thresh = ip.remove_sky(im)
+    assert cv2.imwrite("examples/no_sky/frame%05i.jpg" % i, thresh)
