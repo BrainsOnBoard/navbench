5ee59ad32850a116608426c8b5374880ba917b18
Alex Dewar <a.dewar@sussex.ac.uk>
2020-07-21 15:25:07 +0100

    Tidy up view_database.py

--- view_database.py
+++ view_database.py
@@ -6,7 +6,54 @@ from matplotlib.widgets import Slider
 
 import navbench as nb
 
-BIG_INC = 10  # frames
+SMALL_STEP = 1
+BIG_STEP = 10  # frames
+
+
+class DatabaseViewer(nb.Database):
+    def __init__(self, path):
+        nb.Database.__init__(self, path, fullpath=True)
+        self.figure, self.axes = plt.subplots()
+        self.figure.canvas.mpl_connect('key_press_event', self.key_pressed)
+        self.axes_image = None
+
+        self.axes.set_xticks([])
+        self.axes.set_yticks([])
+
+        axslider = self.figure.add_axes([.25, .1, .65, .03])
+        self.fr_slider = Slider(axslider, 'Frame', 1,
+                                len(self), valstep=SMALL_STEP)
+        self.fr_slider.on_changed(lambda _: self.show_frame())
+
+    def change_frame(self, change):
+        newval = min(len(self), max(1, self.fr_slider.val + change))
+        if newval != self.fr_slider.val:
+            self.fr_slider.set_val(newval)
+            self.show_frame()
+
+    def key_pressed(self, event):
+        if event.key == 'right':
+            self.change_frame(SMALL_STEP)
+        elif event.key == 'left':
+            self.change_frame(-SMALL_STEP)
+        elif event.key == 'up':
+            self.change_frame(BIG_STEP)
+        elif event.key == 'down':
+            self.change_frame(-BIG_STEP)
+
+    def show_frame(self):
+        image = self.read_images(self.fr_slider.val - 1)
+
+        if self.axes_image is None:
+            self.axes_image = self.axes.imshow(image, cmap='gray')
+        else:
+            self.axes_image.set_data(image)
+        self.axes.set_title('Frame %i/%i' % (self.fr_slider.val, len(self)))
+        self.figure.canvas.draw()
+
+    def run(self):
+        self.show_frame()
+        plt.show()
 
 
 def usage():
@@ -17,62 +64,13 @@ def usage():
 def main():
     if len(sys.argv) != 2:
         usage()
-    path = sys.argv[1]
 
     try:
-        db = nb.Database(path, fullpath=True)
+        viewer = DatabaseViewer(sys.argv[1])
     except FileNotFoundError:
         print(sys.argv[1], 'appears to not be a valid database')
         usage()
-
-    global axim
-    axim = None
-    def show_frame():
-        global axim
-        im = db.read_images(sframe.val - 1)
-
-        # If we cache the returned AxisImage it is much faster to update this
-        # rather than calling imshow() again
-        if not axim:
-            axim = ax.imshow(im, cmap='gray')
-        else:
-            axim.set_data(im)
-
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.set_title('Frame %i/%i' % (sframe.val, len(db)))
-        fig.canvas.draw()
-
-    def press(event):
-        if event.key == 'right':
-            if sframe.val < len(db):
-                sframe.set_val(sframe.val + 1)
-                show_frame()
-        elif event.key == 'left':
-            if sframe.val > 1:
-                sframe.set_val(sframe.val - 1)
-                show_frame()
-        elif event.key == 'up':
-            if sframe.val < len(db):
-                sframe.set_val(min(sframe.val + BIG_INC, len(db)))
-                show_frame()
-        elif event.key == 'down':
-            if sframe.val > 1:
-                sframe.set_val(max(1, sframe.val - BIG_INC))
-                show_frame()
-
-    def update(_):
-        show_frame()
-
-    fig, ax = plt.subplots()
-    fig.canvas.mpl_connect('key_press_event', press)
-
-    axslider = fig.add_axes([.25, .1, .65, .03])
-    sframe = Slider(axslider, 'Frame', 1, len(db), valstep=1)
-    sframe.on_changed(update)
-
-    show_frame()
-    plt.show()
+    viewer.run()
 
 
 if __name__ == '__main__':
