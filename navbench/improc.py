813ef23aa13495c6e03ee361ac6fd9e5003c5d7d
Alex Dewar <a.dewar@sussex.ac.uk>
2020-07-22 14:45:37 +0100

    Add view_idf.py tool

--- navbench/improc.py
+++ navbench/improc.py
@@ -1,9 +1,11 @@
 import cv2
 import numpy as np
 
+
 def resize(width, height):
     '''Return a function to resize image to given size'''
     return lambda im: cv2.resize(im, (width, height))
+
 
 def chain(*args):
     '''Return a function which chains the outputs of provided functions'''
@@ -13,14 +15,17 @@ def chain(*args):
         return im
     return chainedfun
 
+
 def histeq(im):
     '''Histogram equalisation: Requires uint8s as input'''
     return cv2.equalizeHist(im)
+
 
 def to_float(im):
     # Normalise values
     info = np.iinfo(im.dtype)
     return im.astype(np.float) / info.max
+
 
 def fill_holes(im):
     '''Fills in any black objects'''
@@ -31,9 +36,12 @@ def fill_holes(im):
         cv2.drawContours(im, [cnt], 0, 255, -1)
     return im
 
+
 def threshold_otsu(im):
-    _, thresh = cv2.threshold(im, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    _, thresh = cv2.threshold(
+        im, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     return thresh
+
 
 def get_ground_mask(im):
     # First, roughly threshold image
@@ -41,7 +49,7 @@ def get_ground_mask(im):
 
     # Set bottom row to white so that any black bits below skyline will be
     # filled in
-    im[-1,:] = 255
+    im[-1, :] = 255
 
     # Fill any black holes
     im = fill_holes(im)
@@ -50,6 +58,7 @@ def get_ground_mask(im):
     im = cv2.bitwise_not(im)
     im = fill_holes(im)
     return cv2.bitwise_not(im)
+
 
 def remove_sky(im):
     '''Sets the sky to 0'''
---
+++ view_idf.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+from argparse import ArgumentParser
+
+import cv2
+import matplotlib.pyplot as plt
+
+import navbench as nb
+from navbench import improc as ip
+
+
+class IDFViewer(nb.Database):
+    def __init__(self, database, goal=None, bound_size=None):
+        nb.Database.__init__(self, database, fullpath=True)
+        self.figure, axes = plt.subplots(4)
+        self.axes_image = None
+        self.axes_diffim = None
+        self.ax_goal, self.ax_imshow, _, self.ax_plot = axes
+        self.ax_diff = axes[2]
+        self.count = 0
+
+        if goal is None:
+            self.goal = goal = len(self) // 2
+        else:
+            self.goal = goal
+        self.frame = goal
+        print('Goal:', goal)
+
+        if bound_size is None:
+            bound_size = 100
+        self.bounds = (max(0, goal - bound_size),
+                       min(len(self) - 1, goal + bound_size))
+        print('Bounds:', self.bounds)
+
+        self.ax_imshow.set_xticks([])
+        self.ax_imshow.set_yticks([])
+        self.figure.canvas.mpl_connect(
+            'button_press_event', self.mouse_clicked)
+
+        resize = ip.resize(180, 55)
+        entries = range(*self.bounds)
+        self.images = self.read_images(entries, resize)
+        self.snap = self.read_images(goal, resize)
+        print(self.images.shape[2], 'images loaded')
+
+        idf = nb.get_route_idf(self.images, self.snap)
+        ca_bounds, goal2 = nb.get_idf_ca_bounds(idf)
+
+        nb.plot_ca(entries, idf, ca_bounds, goal2, ax=self.ax_plot)
+
+        self.ax_goal.set_xticks([])
+        self.ax_goal.set_yticks([])
+        self.ax_goal.imshow(self.snap, cmap='gray')
+
+        self.ax_diff.set_xticks([])
+        self.ax_diff.set_yticks([])
+
+    def mouse_clicked(self, event):
+        if event.inaxes == self.ax_plot and event.button == 1:
+            self.frame = round(event.xdata)
+            self.show_frame()
+
+    def show_frame(self):
+        image = self.images[:, :, self.frame - self.bounds[0]]
+        diffim = cv2.subtract(self.snap, image)
+        diffim = (diffim + 1) / 2
+
+        if self.axes_image is None:
+            self.axes_image = self.ax_imshow.imshow(image, cmap='gray')
+
+            # **HACK**: If we show diffim here then axes_diffim ends up screwed
+            # for some reason...
+            self.axes_diffim = self.ax_diff.imshow(image, cmap='hot')
+        else:
+            self.axes_image.set_data(image)
+            self.axes_diffim.set_data(diffim)
+        self.ax_imshow.set_title('Frame %i/%i' % (self.frame + 1, len(self)))
+        self.figure.canvas.draw()
+
+    def run(self):
+        self.show_frame()
+        plt.show()
+
+
+def main():
+    parser = ArgumentParser(
+        description='A tool for comparing image differences within an image database')
+    parser.add_argument('--goal', nargs='?', type=int, help='goal frame')
+    parser.add_argument('--bound-size', nargs='?', type=int,
+                        help='number of frames either side of goal to test')
+    parser.add_argument('database', help='database path')
+    args = parser.parse_args()
+
+    viewer = IDFViewer(args.database, args.goal, args.bound_size)
+    viewer.run()
+
+
+if __name__ == '__main__':
+    main()
