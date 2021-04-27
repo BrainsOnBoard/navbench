2aa46c6104e3405a5a348f6ea2bdcede4e362c9a
Alex Dewar <a.dewar@sussex.ac.uk>
2021-04-27 17:25:31 +0100

    pytest: for RIDF tests, check that the output is the correct size

--- navbench/test/test_ridf.py
+++ navbench/test/test_ridf.py
@@ -9,25 +9,31 @@ im2[:, 1] = 1
 im3 = np.zeros((5, 4))
 im3[:, 2] = 1
 
+def compare_mat(a, b):
+    a = np.array(a)
+    b = np.array(b)
+    assert a.ndim == b.ndim
+    assert a.shape == b.shape
+    assert (a == b).all()
+
 
 def test_autoridf():
-    assert (ridf(im0, im0) == [0, 0]).all()
-    assert (ridf(im1, im1) == [0, 2/3, 2/3]).all()
+    compare_mat(ridf(im0, im0), [0, 0])
+    compare_mat(ridf(im1, im1), [0, 2/3, 2/3])
 
 
 def test_other():
-    assert (ridf(im1, im2) == [2/3, 0, 2/3]).all()
-    assert (ridf(im2, im1) == [2/3, 2/3, 0]).all()
+    compare_mat(ridf(im1, im2), [2/3, 0, 2/3])
+    compare_mat(ridf(im2, im1), [2/3, 2/3, 0])
 
 
 def test_multi():
-    assert (ridf([im1, im2], im1) == [[0, 2/3, 2/3], [2/3, 2/3, 0]]).all()
+    compare_mat(ridf([im1, im2], im1), [[0, 2/3, 2/3], [2/3, 2/3, 0]])
 
     res = ridf([im1, im1, im2], im1)
-    assert res.shape == (3, 3)
-    assert (res == [[0, 2/3, 2/3], [0, 2/3, 2/3], [2/3, 2/3, 0]]).all()
+    compare_mat(res, [[0, 2/3, 2/3], [0, 2/3, 2/3], [2/3, 2/3, 0]])
 
 
 def test_step2():
-    assert (ridf(im0, im0, step=2) == [0]).all()
-    assert (ridf(im3, im3, step=2) == [0, 1/2]).all()
+    compare_mat(ridf(im0, im0, step=2), [0])
+    compare_mat(ridf(im3, im3, step=2), [0, 1/2])
