63a18c3a25cc96877bfb26cc1e0f92a91ec21511
Alex Dewar <a.dewar@sussex.ac.uk>
2022-01-21 16:22:06 +0000

    Tweaks (forgot heading offset)

--- analysis/compare_bob_navigation.py
+++ analysis/compare_bob_navigation.py
@@ -12,11 +12,11 @@ import pathos.multiprocessing as mp
 from time import time
 
 IM_SIZE = (90, 10)
-DB_ROOT = "datasets/ant_world/routes"
-DB_PATH = DB_ROOT + "/ant1_route1"
+DB_ROOT = "datasets/rc_car"
+DB_PATH = DB_ROOT + "/2020-11-04/unwrapped_dataset1"
 LEARNING_RATE = 0.01
 TANH_SCALING_FACTOR = 0.1
-SEED = None
+SEED = 42
 
 def get_initial_weights(im_size, num_hidden, seed):
     weights, _ = bobnav.InfoMax.generate_initial_weights(
@@ -24,7 +24,7 @@ def get_initial_weights(im_size, num_hidden, seed):
     return weights
 
 
-@nb.cache_result
+# @nb.cache_result
 def get_ann_bobnav(
         im_size, weights, learning_rate, tanh_scaling_factor, db_path):
     algo = bobnav.InfoMax(
@@ -51,12 +51,14 @@ def plot_heads(ax, db, test_entries, heads):
     u = np.cos(heads)
     v = np.sin(heads)
 
-    ax.quiver(x, y, u, v, angles='xy', scale_units='xy', scale=2)
+    ax.quiver(x, y, u, v, angles='xy', scale_units='xy') #, scale=10)
+    ax.plot(x[0], y[0], 'go')
     ax.axis('equal')
 
 
 init_weights = get_initial_weights(
     IM_SIZE, num_hidden=np.prod(IM_SIZE), seed=SEED)
+
 weights = get_ann_bobnav(IM_SIZE, init_weights,
                          LEARNING_RATE, TANH_SCALING_FACTOR, DB_PATH)
 bob_im = bobnav.InfoMax(
@@ -66,20 +68,21 @@ bob_im = bobnav.InfoMax(
 db = nb.Database(DB_PATH)
 train_images = db.read_images(preprocess=ip.resize(*IM_SIZE), to_float=False)
 nb_im = get_ann_nb(weights, LEARNING_RATE, TANH_SCALING_FACTOR, train_images)
+head_offset = db.calculate_heading_offset(0.25)
 
 test_entries = range(0, len(db), 50)
 test_images = [train_images[i] for i in test_entries]
 
 t0 = time()
 with mp.Pool() as pool:
-    bob_heads = pool.map(lambda im: bob_im.get_heading(im), test_images)
+    bob_heads = np.array(pool.map(lambda im: bob_im.get_heading(im), test_images))
 elapsed = time() - t0
 print(f'Took {elapsed} s')
 
 nb_heads = nb.get_infomax_headings(nb_im, test_images)
 
 _, axes = plt.subplots(1, 2)
-plot_heads(axes[0], db, test_entries, nb_heads)
-plot_heads(axes[1], db, test_entries, bob_heads)
+plot_heads(axes[0], db, test_entries, nb_heads + head_offset)
+plot_heads(axes[1], db, test_entries, bob_heads + head_offset)
 
 plt.show()
