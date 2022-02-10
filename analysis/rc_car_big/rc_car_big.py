f49b40ecad97f3442a08c20551a50c6eebe6f487
Alex Dewar <a.dewar@sussex.ac.uk>
2022-02-10 19:35:10 +0000

    Code for getting distances to a training route

---
+++ analysis/rc_car_big/rc_car_big.py
@@ -0,0 +1,14 @@
+import os
+ROOT = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
+import sys
+sys.path.append(ROOT)
+
+import navbench as nb
+from glob import glob
+DBROOT = os.path.join(ROOT, 'datasets/rc_car/rc_car_big')
+
+def get_paths():
+    return glob(os.path.join(DBROOT, 'unwrapped_*'))
+
+def load_databases(paths=get_paths()):
+    return [nb.Database(path) for path in paths]
