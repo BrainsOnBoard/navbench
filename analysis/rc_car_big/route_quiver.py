9cf9a4a4540679e8cebd55e5c46564058022f649
Alex Dewar <a.dewar@sussex.ac.uk>
2022-03-21 11:33:10 +0000

    Pull generic analysis functions out of route_quiver.py

--- analysis/rc_car_big/route_quiver.py
+++ analysis/rc_car_big/route_quiver.py
@@ -1,46 +1,25 @@
 # %%
 import rc_car_big
 import matplotlib.pyplot as plt
-import numpy as np
 import navbench as nb
 
 TRAIN_SKIP = 40
 TEST_SKIP = 40
-TO_FLOAT = False
-
-def get_valid_entries(db, skip):
-    lst = [i for i, x in enumerate(db.x) if not np.isnan(x)]
-    return lst[::skip]
 
 paths = rc_car_big.get_paths()
 dbs = rc_car_big.load_databases(paths[0:6], limits_metres=(0, 100))
 
 train_route = dbs[0]
 test_routes = dbs[1:]
-heading_offset = train_route.calculate_heading_offset(0.25)
 
-train_entries = get_valid_entries(train_route, TRAIN_SKIP)
-train_images = train_route.read_images(train_entries, to_float=TO_FLOAT)
-print(f'Training images: {len(train_images)}')
+analysis = rc_car_big.Analysis(train_route, TRAIN_SKIP)
 
 _, ax0 = plt.subplots()
 ax0.plot(train_route.x, train_route.y)
-
 _, ax1 = plt.subplots()
 
-# TODO: Could do e.g. medianfilt over these headings
-gps_headings = np.arctan2(np.diff(train_route.y), np.diff(train_route.x))
-gps_headings = np.append(gps_headings, [gps_headings[-1]])
-snapshot_headings = gps_headings[train_entries]
-
 for test_route in test_routes:
-    test_entries = get_valid_entries(test_route, TEST_SKIP)
-    test_images = test_route.read_images(test_entries, to_float=TO_FLOAT)
-
-    print(f'Test images: {len(test_images)}')
-
-    headings, best_snaps = nb.get_ridf_headings_and_snap(test_images, train_images)
-    headings += snapshot_headings[best_snaps]
+    test_entries, headings, nearest_train_entries, heading_error = analysis.get_headings(test_route, TEST_SKIP)
 
     label = test_route.name.replace('unwrapped_','')
     lines = ax0.plot(test_route.x, test_route.y, '--', label=label)
@@ -54,10 +33,7 @@ for test_route in test_routes:
         headings, color=colour, zorder=float('inf'), scale=300, scale_units='xy',
         alpha=0.8)
 
-    nearest = train_route.get_nearest_entries(test_route.x[test_entries], test_route.y[test_entries])
-    target_headings = gps_headings[nearest]
-    heading_error = np.abs(nb.normalise180(np.rad2deg(target_headings - headings)))
-    ax1.plot(train_route.distance[nearest], heading_error, label=label)
+    ax1.plot(train_route.distance[nearest_train_entries], heading_error, label=label)
     ax1.set_xlabel("Distance along training route (m)")
     ax1.set_ylabel("Heading error (°)")
 
