654fdbafaf1eae380c846a198c9864138785f7b5
Alex Dewar <a.dewar@sussex.ac.uk>
2022-04-11 14:15:48 +0100

    anglequiver: Allow for inverting y axis
    We need this for plots with satellite images as backgrounds, where the y axis is inverted (i.e. in the normal direction for images).

--- navbench/plotting.py
+++ navbench/plotting.py
@@ -2,7 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def anglequiver(ax, x, y, theta, **kwargs):
+def anglequiver(ax, x, y, theta, invert_y=False, **kwargs):
     u = np.cos(theta)
     v = np.sin(theta)
+    if invert_y:
+        v = -v
     return ax.quiver(x, y, u, v, angles='xy', **kwargs) #, scale_units='xy')
