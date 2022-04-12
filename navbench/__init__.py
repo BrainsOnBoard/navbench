1dcd5726b25ffe924f397a489a799999371b05b3
Alex Dewar <a.dewar@sussex.ac.uk>
2022-04-12 17:36:05 +0100

    Move some python code to bob

--- navbench/__init__.py
+++ navbench/__init__.py
@@ -1,6 +1,8 @@
-from .ca import *
 from .caching import *
 from .database import *
 from .infomax import *
 from .navigation import *
 from .plotting import *
+
+from bob_robotics.navigation.ca import *
+import bob_robotics.navigation.imgproc as imgproc
