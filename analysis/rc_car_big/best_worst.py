dfcbdb49031657908922eb516489766aee998dd7
Alex Dewar <a.dewar@sussex.ac.uk>
2022-04-21 17:39:25 +0100

    Fix up best_worst.py

--- analysis/rc_car_big/best_worst.py
+++ analysis/rc_car_big/best_worst.py
@@ -1,10 +1,11 @@
-import rc_car_big
 import matplotlib.pyplot as plt
+import rc_car_big
+
+import navbench as nb
 from navbench import imgproc as ip
 
 TRAIN_SKIP = 40
 TEST_SKIP = 1000
-# PREPROC = None
 PREPROC = ip.resize(180, 45)
 
 paths = rc_car_big.get_paths()
@@ -27,21 +28,22 @@ def plot_test_point(datum):
     datum = datum.squeeze()
     print('Error: %.2f°' % datum.heading_error)
 
-    _, ax = plt.subplots(3, 1)
+    _, ax = plt.subplots(5, 1)
     test_im = datum.image
-    best_train_entry = analysis.train_entries.loc[datum.best_snap]
+    best_train_entry = analysis.train_entries.loc[datum.best_snap_idx, :]
     train_im = best_train_entry.image
-    nearest_train_entry = analysis.train_route.loc[datum.nearest_train_idx]
-    # diff_im = ip.to_float(test_im) - ip.to_float(train_im)
+    nearest_train_entry = analysis.train_route.read_image_entries(analysis.train_route.loc[datum.nearest_train_idx], preprocess=PREPROC, to_float=False)
     target_snap = analysis.train_route.read_images(datum.nearest_train_idx, preprocess=PREPROC, to_float=False)
-    # target_diff_im = ip.to_float(test_im) - ip.to_float(target_snap)
 
     show_im(ax[0], test_im, 'Test image (%.2f m)' % nearest_train_entry.distance)
     show_im(ax[1], train_im, 'Best-matching snapshot (%.2f m)' % best_train_entry.distance)
-    # show_im(ax[2], diff_im, 'Difference')
-    show_im(ax[2], target_snap, '"Target" snapshot')
-    # show_im(ax[4], target_diff_im, 'Difference')
+    nb.plot_ridf(datum.ridf / 255, ax=ax[2], show_minimum=True)
+    ax[2].set_title(f'RIDF (snapshot={datum.best_snap_idx})')
+    show_im(ax[3], target_snap, '"Target" snapshot')
+    target_ridf = analysis.pm.ridf(nearest_train_entry)
+    nb.plot_ridf(target_ridf.ridf / 255, ax=ax[4], show_minimum=True)
+    ax[4].set_title(f'RIDF (snapshot={nearest_train_entry.database_idx})')
 
-# Show point with highest heading error
+# Show worst match
 plot_test_point(test_df.iloc[-1])
 plt.show()
