7b125f1841d8038f180fc4ad7c60261bffb62c96
Alex Dewar <a.dewar@sussex.ac.uk>
2022-04-25 15:23:51 +0100

    Move plotting code to BoB robotics

--- navbench/__init__.py
+++ navbench/__init__.py
@@ -2,6 +2,6 @@ from .caching import *
 from .database import *
 from .infomax import *
 from .navigation import *
-from .plotting import *
 
 from bob_robotics.navigation.ca import *
+from bob_robotics.navigation.plotting import *
