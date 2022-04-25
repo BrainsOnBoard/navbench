ea9879bd716578074a6a64d14a778bd9f1e7d427
Alex Dewar <a.dewar@sussex.ac.uk>
2022-04-25 16:07:15 +0100

    Move all remaining navbench code to BoB robotics
    Leave a stub there for convenience.

--- navbench/__init__.py
+++ navbench/__init__.py
@@ -1,7 +1,1 @@
-from .database import *
-from .infomax import *
-
-from bob_robotics.navigation.ca import *
-from bob_robotics.navigation.caching import *
-from bob_robotics.navigation.navigation import *
-from bob_robotics.navigation.plotting import *
+from bob_robotics.navigation import *
