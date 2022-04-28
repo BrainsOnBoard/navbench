a630851e121fa61d0a876d69188b00e9e238e1a9
Alex Dewar <a.dewar@sussex.ac.uk>
2022-04-28 16:18:05 +0100

    Tweaks to some plotting code

--- analysis/rc_car_big/best_worst.py
+++ analysis/rc_car_big/best_worst.py
@@ -28,11 +28,12 @@ def get_data(images, snapshots, step=1):
     return pm.ridf(images, step=step)
 
 def show_rot_diffim(ax, im, snap, theta, title):
+    ax.cla()
     rim = np.roll(im, round(theta * im.shape[1] / (2 * np.pi)), axis=1)
     diffim = rim.astype(float) - snap.astype(float)
     diffim /= 255
     diffim = (diffim + 1) / 2
-    show_im(ax, diffim, title, cmap='hot')
+    show_im(ax, diffim, title, cmap=None)
 
 def show_im(ax, im, title, cmap='gray'):
     ax.imshow(im, cmap=cmap)
@@ -40,19 +41,17 @@ def show_im(ax, im, title, cmap='gray'):
     ax.set_yticks(())
     ax.set_title(title)
 
-def show_data(ax, snap, ridf, best_snap_idx, snap_title):
-    show_im(ax[0], test_im, 'Test image (i=%d)' % datum.database_idx)
+def show_data(snap, ridf, snap_title):
+    _, ax = plt.subplots(4, 1)
+    show_im(ax[0], test_im, f'Test image (i={datum.database_idx})')
     show_im(ax[1], snap, snap_title)
     nb.plot_ridf(ridf.ridf / 255, ax=ax[2], show_minimum=True)
-    ax[2].set_title(f'RIDF (snapshot={best_snap_idx})')
-    show_rot_diffim(ax[3], test_im, best_snap, ridf.estimated_dheading, "Difference")
+    show_rot_diffim(ax[3], test_im, snap, ridf.estimated_dheading, "Difference")
 
 # Show worst match
 datum = test_df.iloc[-1]
-datum = datum.squeeze()
 print('Error: %.2f°' % datum.heading_error)
 
-_, ax0 = plt.subplots(4, 1)
 test_im = datum.image
 best_train_entry = analysis.train_entries.loc[datum.best_snap_idx, :]
 best_snap = best_train_entry.image
@@ -60,10 +59,8 @@ nearest_train_entry = analysis.train_route.read_image_entries(analysis.train_rou
 target_snap = analysis.train_route.read_images(datum.nearest_train_idx, preprocess=PREPROC)
 target_ridf = get_data(test_im, target_snap)
 
-show_data(ax0, best_snap, datum, datum.best_snap_idx, 'Best-matching snapshot (%.2f m, j=%d)' % (best_train_entry.distance, best_train_entry.database_idx))
-
-_, ax1 = plt.subplots(4, 1)
-show_data(ax1, target_snap, target_ridf, datum.nearest_train_idx, 'Target snapshot (%.2f m, j=%d)' % (nearest_train_entry.distance, nearest_train_entry.database_idx))
+show_data(best_snap, datum, f'Best-matching snapshot (j={best_train_entry.database_idx})')
+show_data(target_snap, target_ridf, f'Target snapshot (j={nearest_train_entry.database_idx})')
 
 plt.show()
 
