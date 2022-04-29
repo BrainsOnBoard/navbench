1039d4ff8f7a7e0bfbdfa00d84723170f9f48604
Alex Dewar <a.dewar@sussex.ac.uk>
2022-04-29 17:19:25 +0100

    route_quiver: Extra plots

--- analysis/rc_car_big/route_quiver.py
+++ analysis/rc_car_big/route_quiver.py
@@ -9,10 +9,11 @@ import rc_car_big
 import matplotlib.pyplot as plt
 import navbench as nb
 from navbench import imgproc as ip
+from scipy.signal import medfilt
 import numpy as np
 
-TRAIN_SKIP = 40
-TEST_SKIP = 40
+TRAIN_SKIP = 1
+TEST_SKIP = 80
 # PREPROC = None
 PREPROC = ip.resize(45, 180)
 
@@ -21,6 +22,14 @@ dbs = rc_car_big.load_databases(paths[0:4])  #, limits_metres=(0, 200))
 
 train_route = dbs[0]
 test_routes = dbs[1:]
+
+
+def distance2d(entry1, entry2):
+    diff = np.array([entry1.x, entry1.y]) - np.array([entry2.x, entry2.y])
+    if diff.ndim == 1:
+        return np.hypot(*diff)
+    else:
+        return np.hypot(diff[0, :], diff[1, :])
 
 def to_merc(db):
     mlat, mlon = gm_plotting.utm_to_merc(db.x, db.y, 30, 'U')
@@ -33,6 +42,8 @@ analysis = rc_car_big.Analysis(train_route, train_skip=TRAIN_SKIP, preprocess=PR
 _, ax0 = plt.subplots()
 ax0.plot(train_x, train_y, label='Training route')
 _, ax1 = plt.subplots()
+_, ax2 = plt.subplots()
+_, ax3 = plt.subplots()
 
 for test_route in test_routes:
     test_df = analysis.get_headings(test_route, TEST_SKIP)
@@ -50,11 +61,21 @@ for test_route in test_routes:
         alpha=0.8, invert_y=True)
 
     # Cap at 90°
-    test_df.heading_error = np.minimum(test_df.heading_error, 90)
-    ax1.scatter(train_route.distance[test_df.nearest_train_idx], test_df.heading_error,
-                label=label, alpha=0.5, marker='.')
+    err = np.minimum(test_df.heading_error, 90)
+    dist_along_train = train_route.distance[test_df.nearest_train_idx]
+
+    ax1.scatter(dist_along_train, err, label=label, alpha=0.5, marker='.')
     ax1.set_xlabel("Distance along training route (m)")
     ax1.set_ylabel("Heading error (°)")
+
+    ax2.plot(dist_along_train, medfilt(err, kernel_size=5), label=label)
+    ax2.set_xlabel("Distance along training route (m)")
+    ax2.set_ylabel("Heading error (°)")
+
+    dist_to_train = distance2d(test_df, train_route.loc[test_df.nearest_train_idx])
+    ax3.scatter(dist_to_train, err, label=label, alpha=0.5, marker='.')
+    ax3.set_xlabel("Distance to training route (m)")
+    ax3.set_ylabel("Heading error (°)")
 
 gm_plotting.APIClient().add_satellite_image_background(ax0)
 
@@ -63,5 +84,13 @@ ax0.legend()
 ax1.legend()
 ax1.set_xlim(left=0)
 ax1.set_ylim(bottom=0)
+
+ax2.legend()
+ax2.set_xlim(left=0)
+ax2.set_ylim(bottom=0)
+
+ax3.legend()
+ax3.set_xlim(0, 20)
+ax3.set_ylim(bottom=0)
 
 plt.show()
