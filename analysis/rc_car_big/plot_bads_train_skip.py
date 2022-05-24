de7deae8ad854429e3c9d6a1299b3b86dcfd0f5a
Alex Dewar <a.dewar@sussex.ac.uk>
2022-05-24 18:20:58 +0100

    Plot number of bad headings

---
+++ analysis/rc_car_big/plot_bads_train_skip.py
@@ -0,0 +1,48 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import rc_car_big
+
+TRAIN_SKIP = [100, 50, 20, 10, 5, 1]
+TEST_SKIP = 80
+IM_SIZE = (45, 180)
+PREPROC = ['None', 'ip.remove_sky', 'ip.remove_sky_and_histeq']
+
+
+train_dict = dict()
+for proc in PREPROC:
+    _, ax = plt.subplots()
+    train_dict[proc] = {'labels': [], 'data': [], 'ax': ax}
+
+
+class ErrBoxPlot:
+    def __init__(self, analysis, preprocess, params):
+        label = len(analysis.train_entries)
+        train_dict[params['preprocess']]['labels'].append(label)
+
+    def on_test(self, train_route, test_route, df, preprocess, params):
+        prop_bads = np.mean(df.heading_error > 45)
+        train_dict[params['preprocess']]['data'].append(prop_bads)
+
+    def on_finish(self, params):
+        cur = train_dict[params['preprocess']]
+        if len(cur['data']) == len(TRAIN_SKIP):
+            ax = cur['ax']
+            ax.plot(cur['labels'], cur['data'])
+            ax.set_ylim(bottom=0)
+            ax.set_xlabel('Number of training images')
+            ax.set_title(f'Preprocessing: {params["preprocess"]}')
+
+
+paths = rc_car_big.get_paths()
+
+
+rc_car_big.run_analysis(
+    paths[0:1],
+    [paths[5]],
+    TRAIN_SKIP,
+    [TEST_SKIP],
+    [IM_SIZE],
+    PREPROC,
+    runner_classes=[ErrBoxPlot])  #, do_export_mats=True)
+plt.show()
